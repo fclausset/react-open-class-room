7eac7fe942c7cec20100ba4c303268359bea3076
fclausset <frederic.clausset@gmail.com>
2018-06-13 17:37:29 +0200

    Build a ticking clock component typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Clock from './Clock';
 import registerServiceWorker from './registerServiceWorker';
 
 let name="Stouty";
