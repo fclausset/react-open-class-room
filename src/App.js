75bc2a8ad662a5c8cf56aaf32ea1fb31146bf826
fclausset <frederic.clausset@gmail.com>
2018-06-14 12:50:36 +0200

    change color

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from './Clock';
+import ColoredBlock from './ColoredBlock.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hi {this.props.name}!
-        </p>
-        <Clock ></Clock> 
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>My React App</h1>
+          </header>
+        <div className="main-content">
+          <ColoredBlock />
+        </div>
+        <footer>...</footer>
       </div>
     );
   }
